cad5bfbc18ccd081c03476f1f452cc78ccc2499e
llabad39 <lucaslabadens@hotmail.fr>
2017-04-07 13:15:16 +0200

    moving with good images

--- src/GameLoop.java
+++ src/GameLoop.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.beans.property.LongProperty;
@@ -11,14 +9,10 @@ import javafx.util.Duration;
 
 public class GameLoop extends Application{
 	private static final int COLUMNS  =     3;
-	private static final int COUNT    =     3;
-	private static final int OFFSET_X =     94;
-	private static final int WALK_RIGHT_OFFSET_Y =    38;
-	private static final int WALK_LEFT_OFFSET_Y =    112;
-	private static final int WALK_UP_OFFSET_Y =    5;
-	private static final int WALK_DOWN_OFFSET_Y =    76;
-	private static final int WIDTH    =    32;
-	private static final int HEIGHT   =    32;
+	    private static final int OFFSET_X =     0;
+	    private static final int WALK_BASE = 0;
+	    private static final int WIDTH    =    32;
+	    private static final int HEIGHT   =    32;
 	Vampire v;
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -27,9 +21,9 @@ public class GameLoop extends Application{
 		pane.setPrefSize(1024,640);
 		Scene scene = new Scene(pane);
 		v = new Vampire(Duration.millis(500),COLUMNS,
-				OFFSET_X, WALK_RIGHT_OFFSET_Y,
-				WIDTH, HEIGHT,2,3,0,0,pane,0);
-		pane.getChildren().addAll(v.getCurrentView());
+				OFFSET_X, WALK_BASE,
+				WIDTH, HEIGHT,0,3,0,0,pane,0);
+		pane.getChildren().add(0,v.getCurrentView());
 		final LongProperty lastUpdateTime = new SimpleLongProperty();
 
 		final AnimationTimer rectangleAnimation = new AnimationTimer() {
@@ -39,8 +33,11 @@ public class GameLoop extends Application{
 				// TODO Auto-generated method stub
 
 				if (lastUpdateTime.get() > 0) {
-					final double elapsedSeconds = (now - lastUpdateTime.get()) / 1_000_000_000.0 ;
+					pane.getChildren().remove(0);
 					v.move();
+					v.play();
+					pane.getChildren().add(0,v.getCurrentView());
+					
 				}
                 lastUpdateTime.set(now);
 			}
--- src/Vampire.java
+++ src/Vampire.java
@@ -10,16 +10,15 @@ public class Vampire extends Character {
 	public Vampire(Duration duration, int columns, int offsetX, int offsetY, int width,
 			int height, int dir, int life,int posX,int posY,Pane p,int id) {
 		super(duration, columns, offsetX, offsetY, width, height, dir, life,posX,posY,p);
-		imageView = new ImageView[3];
+		imageView = new ImageView[4];
 		this.id = id;
-		for(int i=0;i<3;i++){
+		for(int i=0;i<4;i++){
 			imageView[i]  = new ImageView(super.character);
-			imageView[i].setViewport(new Rectangle2D(offsetX+i*width,offsetY,width,height));
+			imageView[i].setViewport(new Rectangle2D(offsetX,offsetY+i*(height+3),width,height));
 		}
 	}
 
 	void move(){
-		pane.getChildren().removeAll();
 		int t = (int) (Math.random()*4);
 		switch(t){
 		case 0: 
@@ -36,25 +35,19 @@ public class Vampire extends Character {
 			moveRight();
 			break;
 		}
-		pane.getChildren().addAll(getCurrentView());
-		this.play();
+
 		getCurrentView().setTranslateX(posX);
 		getCurrentView().setTranslateY(posY);
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 		
 	}
 	
 	void moveUp(){
 		super.animCurrent = 0;
-
 		if(posY >=50){
 			posY = posY-50;
 		}
+		this.play();
 	}
 	void moveLeft(){
 		super.animCurrent = 3;
@@ -63,7 +56,8 @@ public class Vampire extends Character {
 			posX = posX-50;
 
 		}
-		
+		this.play();
+
 
 	}
 	void moveRight(){
@@ -71,7 +65,6 @@ public class Vampire extends Character {
 
 		if(posX <= 1074){
 			posX =posX +50;
-			System.out.println("on à bouger");
 		}
 
 	}
